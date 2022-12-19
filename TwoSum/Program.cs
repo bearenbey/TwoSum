975ab193af453f1dffa30c523c6f10dc4d9dbe6d
Eren Ogrul <bearen@Erens-MacBook-Pro.local>
2022-12-19 20:53:18 +0100

    Initial commit

---
+++ TwoSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Given an array of integers nums and an integer target, return
+             * indices of the two numbers such that they add up to target. You
+             * may assume that each input would have exactly one solution, and
+             * you may not use the same element twice.
+             */
+            
+            Console.Write("Please enter the list of numbers with comma separated: ");
+            string userInput = Console.ReadLine();
+            
+            var num = userInput.TrimStart('[').TrimEnd(']').Split(',');
+            int[] numList = Array.ConvertAll(num, int.Parse);
+
+            Console.Write("What should be the indice of two numbers? ");
+            int userIndice = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numList.Length-1; i++)
+            {
+                if (numList[i] + numList[i+1] == userIndice)
+                {
+                    Console.WriteLine($"[{i}, {i+1}]");
+                }
+            }
+        }
+    }
+}
